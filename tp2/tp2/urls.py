0ad1af62adc47ae3b39dd2aa057b97ed12b0787f
Xavier Santos <xavier@ua.pt>
2019-12-18 16:39:08 +0000

    URLS.PY UPDATED

--- tp2/tp2/urls.py
+++ tp2/tp2/urls.py
@@ -13,12 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, re_path
 
 from app import views
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='static/ico/favicon.ico', permanent=True)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^favicon\.ico$', favicon_view),
     re_path(r'^$', views.home, name='home'),
     re_path(r'^about/$', views.about, name='about'),
+    path('add_org/', views.add_org, name='add_org'),
     path('<path:selection>', views.element, name='element'),
 ]
